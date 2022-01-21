85d16d33f254ac96908d55b5e7c3915975506792
NSChahna <srilathachahna@gmail.com>
2022-01-21 11:15:38 +0530

    changed db

--- function.php
+++ function.php
@@ -13,14 +13,14 @@ body {
 <?php
 function makeconnection()
 {
-	$cn=mysqli_connect("localhost","root","","sourcecodester_travel");
+	$cn=mysqli_connect("localhost","root","","travel");
 	if(mysqli_connect_errno())
 	{
 		echo "failed to connect to mysqli:".mysqli_connect_error();
 	}
 	return $cn;
 }
-$cn=mysqli_connect("localhost","root","","sourcecodester_travel");
+$cn=mysqli_connect("localhost","root","","travel");
 ?>
 </body>
 </html>
