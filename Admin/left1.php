e55f2dc3da4871586b6f5d2f76ce057c2d128134
NSChahna <srilathachahna@gmail.com>
2022-03-26 12:22:28 +0530

    changed

--- Admin/left1.php
+++ Admin/left1.php
@@ -11,7 +11,7 @@
 <table style="width:100%">
 <tr><td style="font-size:28px">User Links</td></tr>
 
-<?php if($_SESSION["usertype"]=="User")
+<?php if($_SESSION["usertype"]=="General")
 {?>
 <tr><td><a href="addcategory.php">Add Category</a></td></tr>
 <tr><td><a href="viewcategory.php">View Category</a></td></tr>
@@ -21,7 +21,7 @@
 <tr><td><a href="addpackage.php">Add Package</a></td></tr>
 <?php }?>
 
-<?php if($_SESSION["usertype"]=="User")
+<?php if($_SESSION["usertype"]=="General")
 {?>
 <tr><td><a href="updatepackage.php">Update Package</a></td></tr>
 <tr><td><a href="deletepackage.php">Delete Package</a></td></tr>
